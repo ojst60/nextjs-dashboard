6ae416f28537158b32f839c37bb753e5378ff7f5
Julius Oyovwikigho <julius.dev1991@gmail.com>
2024-12-06 01:27:22 +0000

    reove link to test

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -1,7 +1,6 @@
-'use client'
+"use client";
 
 import {
-  UserGroupIcon,
   HomeIcon,
   DocumentDuplicateIcon,
 } from "@heroicons/react/24/outline";
@@ -18,7 +17,6 @@ const links = [
     href: "/dashboard/invoices",
     icon: DocumentDuplicateIcon,
   },
-  { name: "Customers", href: "/dashboard/customers", icon: UserGroupIcon },
 ];
 
 export default function NavLinks() {
